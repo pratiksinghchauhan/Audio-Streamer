d41521893d0f0eacb3dc4771a6176686815ad46f
Pratik Singh Chauhan <pratiksingh773@gmail.com>
2017-07-05 17:01:47 +0530

    transcript set to null

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var express=require('express');
-var app=express();
-var https=require('https');
-var fs=require('fs');
-var path=require('path');
+var express = require('express');
+var app = express();
+var https = require('https');
+var fs = require('fs');
+var path = require('path');
 var binaryServer = require('binaryjs').BinaryServer;
 var wav = require('wav');
-var request=require('request')
-var bodyParser=require('body-parser');
+var request = require('request')
+var bodyParser = require('body-parser');
 
 var assert = require('assert');
 var MongoClient = require('mongodb').MongoClient;
@@ -25,151 +25,155 @@ const speech = Speech();
 
 
 var options = {
-    key:    fs.readFileSync('ssl/wildcard_policybazaar_com.key'),
-    cert:   fs.readFileSync('ssl/wildcard_policybazaar_com.crt'), 
+    key: fs.readFileSync('ssl/wildcard_policybazaar_com.key'),
+    cert: fs.readFileSync('ssl/wildcard_policybazaar_com.crt'),
 };
 
+var contexts = fs.readFileSync('contexts.txt', 'utf8');
+contexts = contexts.split(",");
+//console.log(typeof(contexts));
 
-var insertDocument = function( db, msg, callback) {              
-               console.log(msg['leadID']);
+var insertDocument = function(db, msg, callback) {
+    console.log(msg['leadID']);
 
-                db.collection('textResponse').findOne({leadid:msg['leadID']}, {sort: {ts:-1}}, function(err, lastlead) {
-                    console.log(lastlead);
-                     if (lastlead  && lastlead.leadid){
-                    console.log("leadid was found");
-                    var now = new Date();
-                    var lastresponse = lastlead.ts;
-                    var diff = now - lastresponse ;
-                     
-                     if(diff < 120000){
-                         console.log("inside 120 sec");
-                         text=lastlead.speech+msg['speech'];
-                         console.log(text);
-                         db.collection('textResponse').updateOne({_id:lastlead._id},{$set:{speech:text,ts:new Date()}});
-                         console.log("updated");
+    db.collection('textResponse').findOne({ leadid: msg['leadID'] }, { sort: { ts: -1 } }, function(err, lastlead) {
+        console.log(lastlead);
+        if (lastlead && lastlead.leadid) {
+            console.log("leadid was found");
+            var now = new Date();
+            var lastresponse = lastlead.ts;
+            var diff = now - lastresponse;
 
-                     }else{
-                        console.log("outside 120 sec");
-                            db.collection('textResponse').insertOne( {
-                            "speech": msg['speech'],
-                            "leadid":msg['leadID'],
-                            ts: new Date()
-                        })
-                     }
-                }else{
-                        db.collection('textResponse').insertOne({
-                        "speech": msg['speech'],
-                        "leadid":msg['leadID'],
-                        ts: new Date()
-                    })
-                    console.log("data saved");
-                }
-                });
-            };
+            if (diff < 120000) {
+                console.log("inside 120 sec");
+                text = lastlead.speech + msg['speech'];
+                console.log(text);
+                db.collection('textResponse').updateOne({ _id: lastlead._id }, { $set: { speech: text, ts: new Date() } });
+                console.log("updated");
 
-var server=https.createServer(options,app);
+            } else {
+                console.log("outside 120 sec");
+                db.collection('textResponse').insertOne({
+                    "speech": msg['speech'],
+                    "leadid": msg['leadID'],
+                    ts: new Date()
+                })
+            }
+        } else {
+            db.collection('textResponse').insertOne({
+                "speech": msg['speech'],
+                "leadid": msg['leadID'],
+                ts: new Date()
+            })
+            console.log("data saved");
+        }
+    });
+};
 
-server.listen(9191,function(){
-  console.log("Listening at port 9191");
+var server = https.createServer(options, app);
+
+server.listen(9191, function() {
+    console.log("Listening at port 9191");
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-var server = binaryServer({server: server});
+var server = binaryServer({ server: server });
 
 
 
-server.on('connection', function(client){
+server.on('connection', function(client) {
 
-	const encoding = 'LINEAR16';
+    const encoding = 'LINEAR16';
     const sampleRateHertz = 44100;
     const languageCode = 'hi-IN';
 
 
 
-  const request = {
-  config: {
-    encoding: encoding,
-    sampleRateHertz: sampleRateHertz,
-    languageCode: languageCode,
-   speechContexts: {
-    phrases:["good morning","policybazaar.com","policybazaar","sir", "health","insurance","policy","right","age", "good", "morning","nineteen","ninety seven","forty","thirty eight"]
-},
-  },
-  verbose:true,
-  interimResults: true, // If you want interim results, set this to true
-};
+    const request = {
+        config: {
+            encoding: encoding,
+            sampleRateHertz: sampleRateHertz,
+            languageCode: languageCode,
+            speechContexts: {
+                phrases: contexts
+            },
+        },
+        verbose: true,
+        interimResults: true, // If you want interim results, set this to true
+    };
 
 
-   client.on('error', console.error)
-         .on('stream', function(stream,obj) {
-           console.log("Stream started");
-           console.log(obj['leadID']);
+    client.on('error', console.error)
+        .on('stream', function(stream, obj) {
+            console.log("Stream started");
+            console.log(obj['leadID']);
 
-           leadID=obj['leadID']
-
-
+            leadID = obj['leadID']
 
 
 
-  stream.pipe(speech.createRecognizeStream(request)
-  .on('error', console.error)
-  .on('data', (data) =>{ 
 
-      console.log("data recieved")
-      //client.send(data);
-      
-     // process.stdout.write(data.results),
-     
-    /*function rerec(){
-           console.log("restreaming");
-           stream.pipe();
-       }*/
-if( typeof data.results[0] != 'undefined'){
-       transcript=data.results[0].transcript;
-       console.log(transcript);
-}
-      
-     // console.log(data.results);     
 
-if( typeof data.results[0] != 'undefined'){
-       console.log(data.results[0].isFinal);  
+            stream.pipe(speech.createRecognizeStream(request)
+                .on('error', console.error)
+                .on('data', (data) => {
 
-          if(data.results[0].isFinal==true){  
-            console.log("FINAL:" + data.results[0].transcript);
-            fs.appendFile('results.txt',"FINAL:" + data.results[0].transcript +'\n');
-            savemongo(data.results[0].transcript);  
-}}}
-));
+                    console.log("data recieved")
+                        //client.send(data);
 
-         });
-    
-    function savemongo(trans){     
-         if( typeof trans != 'undefined'){
-            MongoClient.connect( url, function(err, db) {
-            console.log(trans);
-            console.log('save mongo');
-            msg={
-                "speech":trans,
-                "leadID":leadID,
-            }    
-            insertDocument(db ,msg)
+                    // process.stdout.write(data.results),
 
-            })}
-        else
-          console.log("savemongo called but nothing to insert");         
-     }
+                    /*function rerec(){
+                           console.log("restreaming");
+                           stream.pipe();
+                       }*/
+                    if (typeof data.results[0] != 'undefined') {
+                        transcript = data.results[0].transcript;
+                        console.log(transcript);
+                    }
 
-    client.on('close',function(){
-          console.log("connection closed from client side");
-          savemongo(transcript);
-      });
- 
+                    // console.log(data.results);     
 
-  console.log('Listening, press Ctrl+C to stop.');
-	
+                    if (typeof data.results[0] != 'undefined') {
+                        console.log(data.results[0].isFinal);
+
+                        if (data.results[0].isFinal == true) {
+                            console.log("FINAL:" + data.results[0].transcript);
+                            fs.appendFile('results.txt', "FINAL:" + data.results[0].transcript + '\n');
+                            savemongo(data.results[0].transcript);
+                        }
+                    }
+                }));
+
+        });
+
+    function savemongo(trans) {
+        if (trans == null)
+            return true;
+
+        if (typeof trans != 'undefined') {
+            MongoClient.connect(url, function(err, db) {
+                console.log(trans);
+                console.log('save mongo');
+                msg = {
+                    "speech": trans,
+                    "leadID": leadID,
+                }
+                insertDocument(db, msg)
+
+            })
+        } else
+            console.log("savemongo called but nothing to insert");
+    }
+
+    client.on('close', function() {
+        console.log("connection closed from client side");
+        savemongo(transcript);
+        transcript = null;
+    });
+
+
+    console.log('Listening, press Ctrl+C to stop.');
+
 });
-
-
-
-
--- public/js/site.js
+++ public/js/site.js
@@ -1,11 +1,9 @@
-
-
-$(function () {
+$(function() {
 
 
 
-     startstream=document.getElementById("start-rec-btn");
-     stopstream=document.getElementById("stop-rec-btn")
+    startstream = document.getElementById("start-rec-btn");
+    stopstream = document.getElementById("stop-rec-btn")
 
 
     var client,
@@ -13,37 +11,37 @@ $(function () {
         context,
         bStream,
         contextSampleRate = (new AudioContext()).sampleRate;
-        resampleRate = contextSampleRate,
+    resampleRate = contextSampleRate,
         worker = new Worker('js/worker/resampler-worker.js');
 
-    var flag=0;
-    var myvar=0;
+    var flag = 0;
+    var myvar = 0;
 
-    worker.postMessage({cmd:"init",from:contextSampleRate,to:resampleRate});  
+    worker.postMessage({ cmd: "init", from: contextSampleRate, to: resampleRate });
 
     window.addEventListener('message', function(msg) {
         console.log(msg);
-        if( msg && msg.data.event == 'startrecording' ){
+        if (msg && msg.data.event == 'startrecording') {
             console.log('inside client startrecording');
             startstream.click();
             leadid = msg.data.leadid;
-        }else if( msg && msg.data.event == 'stoprecording' ){
+        } else if (msg && msg.data.event == 'stoprecording') {
             console.log('inside client stoprecording');
             stopstream.click();
         }
     }, false);
 
-    worker.addEventListener('message', function (e) {
+    worker.addEventListener('message', function(e) {
         if (bStream && bStream.writable)
             bStream.write(convertFloat32ToInt16(e.data.buffer));
     }, false);
 
-    function startrecording(){
+    function startrecording() {
         close();
         console.log('startrecording called');
-        client = new BinaryClient('wss://'+location.host);
-        client.on('open', function () {
-            bStream = client.createStream({sampleRate: resampleRate,leadID:leadid});
+        client = new BinaryClient('wss://' + location.host);
+        client.on('open', function() {
+            bStream = client.createStream({ sampleRate: resampleRate, leadID: leadid });
         });
         if (context) {
             recorder.connect(context.destination);
@@ -53,7 +51,7 @@ $(function () {
             audio: true,
             video: false
         };
-        navigator.getUserMedia(session, function (stream) {
+        navigator.getUserMedia(session, function(stream) {
             context = new AudioContext();
             var audioInput = context.createMediaStreamSource(stream);
             var bufferSize = 0; // let implementation decide
@@ -66,31 +64,30 @@ $(function () {
 
             recorder.connect(context.destination);
 
-        }, function (e) {
-        });
+        }, function(e) {});
     }
 
-    $("#start-rec-btn").click(function () {      
-        if(flag==1){
-           return;
+    $("#start-rec-btn").click(function() {
+        if (flag == 1) {
+            return;
         }
-        flag=0;
-         startrecording();
-    
-        myvar=setInterval(   
-        function reRec(){      
-            console.log("restreaming");
-            startstream.click();
-        }, 50000);
+        flag = 0;
+        startrecording();
+
+        myvar = setInterval(
+            function reRec() {
+                console.log("restreaming");
+                startstream.click();
+            }, 50000);
 
         console.log(flag);
 
-        });
+    });
 
     function onAudio(e) {
         var left = e.inputBuffer.getChannelData(0);
 
-        worker.postMessage({cmd: "resample", buffer: left});
+        worker.postMessage({ cmd: "resample", buffer: left });
 
         drawBuffer(left);
     }
@@ -110,7 +107,7 @@ $(function () {
             height = canvas.height,
             context = canvas.getContext('2d');
 
-        context.clearRect (0, 0, width, height);
+        context.clearRect(0, 0, width, height);
         var step = Math.ceil(data.length / width);
         var amp = height / 2;
         for (var i = 0; i < width; i++) {
@@ -127,28 +124,27 @@ $(function () {
         }
     }
 
-    $("#stop-rec-btn").click(function () {
-        flag=1;
+    $("#stop-rec-btn").click(function() {
+        flag = 1;
         console.log("above clear interval");
-        clearInterval(myvar,function(){
+        clearInterval(myvar, function() {
             console.log("interval cleared");
-            flag=0;
+            flag = 0;
         });
         close();
     });
 
-    function close(){
+    function close() {
         console.log('close');
-        if(recorder)
+        if (recorder)
             recorder.disconnect();
-        if(client)
+        if (client)
             client.close();
     }
-    
+
 });
 
 navigator.getUserMedia = navigator.getUserMedia ||
     navigator.webkitGetUserMedia ||
     navigator.mozGetUserMedia ||
     navigator.msGetUserMedia;
-
